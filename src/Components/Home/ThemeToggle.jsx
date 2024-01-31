677c3d87abb7af184a0cb556fbfcf6a0ca78ea3c
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2024-01-31 21:24:22 +0700

    add menu side

--- src/Components/Home/ThemeToggle.jsx
+++ src/Components/Home/ThemeToggle.jsx
@@ -6,8 +6,7 @@ import Section2 from './Section2';
 import Section3 from './Section3';
 import WaveFooter from './Footer';
 import ParticlesBg from './ParticlesBg';
-
-
+import Menu from './Menu';
 
 const ThemeToggle = () => {
   const storedDarkMode = localStorage.getItem('darkMode');
@@ -16,29 +15,26 @@ const ThemeToggle = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
 
-
   useEffect(() => {
     localStorage.setItem('darkMode', isDarkMode);
     document.body.style.backgroundColor = isDarkMode ? '#333333' : '#ffffff';
   }, [isDarkMode]);
 
-
-
-
   return (
     <StyleSheetManager shouldForwardProp={(prop) => prop !== 'isDarkMode'} style={{ minWidth: "500px" }}>
       <>
+        <Menu isDarkMode={isDarkMode} />
         <TopNav isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
         <ParticlesBg isDarkMode={isDarkMode} />
         <Section1 isDarkMode={isDarkMode} />
         <Section2 isDarkMode={isDarkMode} />
         <Section3 isDarkMode={isDarkMode} />
         <WaveFooter isDarkMode={isDarkMode} />
+
       </>
     </StyleSheetManager>
   );
 };
 
 export default ThemeToggle;
-
 
