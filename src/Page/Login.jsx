9d0cc360927c3bf49ea1d72faec599cced60d074
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2024-02-03 01:41:40 +0700

    fix login

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -50,17 +50,9 @@ export default function SignInSide() {
       return;
     }
 
-    await axios.post('https://vercel.com/kanthorn-wos-projects/server-portfolio/api/login/', doLogin, { withCredentials: true })
+    await axios.post('https://vercel.com/kanthorn-wos-projects/server-portfolio/api/login/', doLogin)
       .then((res) => {
         console.log('Login.jsx - Server Response:', res.data);
-
-        if (!res.data || !res.data.token || !res.data.payload || !res.data.payload.user) {
-          // Handle unexpected response structure
-          console.error('Unexpected server response structure:', res.data);
-          toast.error('Unexpected server response structure. Please try again.');
-          return;
-        }
-
         const token = res.data.token;
         const { id, name, role, active } = res.data.payload.user;
 
